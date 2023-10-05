f88938d2c9b59a5e30e56d1be229fdd4f28ce10f
Hugo Meza <hugo.meza.macias@gmail.com>
2023-10-05 13:10:02 +0200

    LeetCode 2878. Introduction to Pandas. Get the size of a dataframe

---
+++ LeetCode/IntroductionToPandas/2878-get-the-size-of-a-dataframe.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from typing import List
+
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
+
+
+data = [
+    [846, 'Mason', 21,  'Forward', 'RealMadrid'],
+    [749, 'Riley', 30,  'Winger', 'Barcelona'],
+    [155, 'Bob', 28,  'Striker', 'ManchesterUnited'],
+    [583, 'Isabella', 32,  'Goalkeeper', 'Liverpool'],
+    [388, 'Zachary', 24,  'Midfielder', 'BayernMunich'],
+    [883, 'Ava', 23,  'Defender', 'Chelsea'],
+    [355, 'Violet', 18,  'Striker', 'Juventus'],
+    [247, 'Thomas', 27,  'Striker', 'ParisSaint-Germain'],
+    [761, 'Jack', 33,  'Midfielder', 'ManchesterCity'],
+    [642, 'Charlie', 36,  'Center-back', 'Arsenal'],
+]
+
+p = pd.DataFrame(data, columns=['player_id', 'name', 'age', 'position', 'team'])
+print(getDataframeSize(p))
